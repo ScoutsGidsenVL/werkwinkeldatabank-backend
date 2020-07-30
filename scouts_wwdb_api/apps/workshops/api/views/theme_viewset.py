bb312e818da3c8d43156794fcf9ff14eebebb7c9
Jonas <jonas.elst@inuits.eu>
2020-07-30 17:22:06 +0200

    Added request and response bodies to swagger docs

--- scouts_wwdb_api/apps/workshops/api/views/theme_viewset.py
+++ scouts_wwdb_api/apps/workshops/api/views/theme_viewset.py
@@ -1,12 +1,19 @@
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
-from ..serializers.theme_serializers import ThemeCreateInputSerializer, ThemeDetailOutputSerializer, ThemeListOutputSerializer
+from drf_yasg.utils import swagger_auto_schema
+from ..serializers.theme_serializers import (
+    ThemeCreateInputSerializer,
+    ThemeDetailOutputSerializer,
+    ThemeListOutputSerializer,
+    ThemeUpdateInputSerializer,
+)
 from ...services.theme_service import theme_create
 from ...models import Theme
 
 
 class ThemeViewSet(viewsets.ViewSet):
+    @swagger_auto_schema(responses={status.HTTP_200_OK: ThemeDetailOutputSerializer})
     def retrieve(self, request, pk=None):
         # theme = get_object_or_404(Theme.objects, pk=pk)
         theme = Theme.objects.get(pk=pk)
@@ -15,6 +22,9 @@ class ThemeViewSet(viewsets.ViewSet):
 
         return Response(serializer.data)
 
+    @swagger_auto_schema(
+        request_body=ThemeCreateInputSerializer, responses={status.HTTP_201_CREATED: ThemeDetailOutputSerializer}
+    )
     def create(self, request):
         input_serializer = ThemeCreateInputSerializer(data=request.data)
         input_serializer.is_valid(raise_exception=True)
@@ -25,12 +35,16 @@ class ThemeViewSet(viewsets.ViewSet):
 
         return Response(output_serializer.data, status=status.HTTP_201_CREATED)
 
+    @swagger_auto_schema(responses={status.HTTP_200_OK: ThemeListOutputSerializer})
     def list(self, request):
         themes = Theme.objects.all()
         serializer = ThemeListOutputSerializer(themes, many=True)
 
         return Response(serializer.data)
 
+    @swagger_auto_schema(
+        request_body=ThemeUpdateInputSerializer, responses={status.HTTP_200_OK: ThemeDetailOutputSerializer}
+    )
     def partial_update(self, request, pk=None):
         theme = Theme.objects.get(pk=pk)
         serializer = ThemeUpdateInputSerializer(theme)
