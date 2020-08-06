f7c4a55ef06d325038e139a2bb456de968b2d7c4
Jonas <jonas.elst@inuits.eu>
2020-08-06 09:43:34 +0200

    Optimized workshop create without theme get

--- scouts_wwdb_api/apps/workshops/services/workshop_service.py
+++ scouts_wwdb_api/apps/workshops/services/workshop_service.py
@@ -1,13 +1,11 @@
 from ..models import Workshop
 from ..models import Theme
-from django.shortcuts import get_object_or_404
 import uuid
 
 
 def workshop_create(*, title: str, duration: str, theme: str, description: str, necessities: str) -> Workshop:
-    theme_object = get_object_or_404(Theme.objects, pk=theme)
     workshop = Workshop(
-        title=title, duration=duration, theme=theme_object, description=description, necessities=necessities
+        title=title, duration=duration, theme_id=theme, description=description, necessities=necessities
     )
     workshop.full_clean()
     workshop.save()
