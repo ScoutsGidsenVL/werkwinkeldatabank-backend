cffd1537de5f6c8db4f83699252a2e56296a3b58
Jonas <jonas.elst@inuits.eu>
2020-08-20 16:21:27 +0200

    Added filter extensions to allow for multiple uuid filter

--- scouts_wwdb_api/apps/workshops/api/filters/workshop_filter.py
+++ scouts_wwdb_api/apps/workshops/api/filters/workshop_filter.py
@@ -1,10 +1,11 @@
 import django_filters
 from ...models import Workshop
+from apps.filter_extensions.filters import MultipleUUIDFilter
 
 
 class WorkshopFilter(django_filters.FilterSet):
     term = django_filters.CharFilter(field_name="title", lookup_expr="icontains")
-    theme = django_filters.UUIDFilter(lookup_expr="exact")
+    theme = MultipleUUIDFilter()
 
     class Meta:
         model = Workshop
