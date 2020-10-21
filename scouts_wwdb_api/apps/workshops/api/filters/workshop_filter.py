d52036acf62810591e577e310d4f1c1d4a384009
Jonas <jonas.elst@inuits.eu>
2020-10-21 13:59:22 +0200

    Added status en created_by filter to workshop

--- scouts_wwdb_api/apps/workshops/api/filters/workshop_filter.py
+++ scouts_wwdb_api/apps/workshops/api/filters/workshop_filter.py
@@ -1,5 +1,6 @@
 import django_filters
 from ...models import Workshop
+from ...models.enums import WorkshopStatusType
 from apps.filter_extensions.filters import MultipleUUIDFilter
 from apps.base.filters import ActiveFilterMixin
 
@@ -10,6 +11,8 @@ class WorkshopFilter(ActiveFilterMixin, django_filters.FilterSet):
     duration_start = django_filters.DurationFilter(field_name="duration", lookup_expr="gte")
     duration_end = django_filters.DurationFilter(field_name="duration", lookup_expr="lte")
     is_sensitive = django_filters.BooleanFilter(method="is_sensitive_filter")
+    status = django_filters.MultipleChoiceFilter(choices=WorkshopStatusType.choices, field_name="workshop_status_type")
+    created_by = MultipleUUIDFilter()
 
     def is_sensitive_filter(self, queryset, name, value):
         if value:
