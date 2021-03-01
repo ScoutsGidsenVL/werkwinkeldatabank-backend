d719dea97c9929b8242bfd2799b9cac116dcb338
ricardo <ricardo@inuits.eu>
2021-03-01 13:55:08 +0100

    Ref #73977, Adjusted group list in scouts_team.py

--- scouts_wwdb_api/apps/workshops/models/enums/scouts_team.py
+++ scouts_wwdb_api/apps/workshops/models/enums/scouts_team.py
@@ -2,5 +2,20 @@ from django.db import models
 
 
 class ScoutsTeam(models.TextChoices):
-    GROUP1 = "GROUP1", "Ploeg 1"
-    GROUP2 = "GROUP2", "Ploeg 2"
+    GROUP1 = "GROUP1", "Vorming"
+    GROUP2 = "GROUP2", "Diversiteit"
+    GROUP3 = "GROUP3", "Ecologie"
+    GROUP4 = "GROUP4", "Internationaal"
+    GROUP5 = "GROUP5", "IT"
+    GROUP6 = "GROUP6", "Lokalen"
+    GROUP7 = "GROUP7", "Technieken"
+    GROUP8 = "GROUP8", "Zingeving"
+    GROUP9 = "GROUP9", "Kapoenen"
+    GROUP10 = "GROUP10", "Kabouters & Welpen"
+    GROUP11 = "GROUP11", "Jonggivers"
+    GROUP12 = "GROUP12", "Givers"
+    GROUP13 = "GROUP13", "Jins"
+    GROUP14 = "GROUP14", "Groepsleiding"
+    GROUP15 = "GROUP15", "Akabe"
+    GROUP16 = "GROUP16", "Zeescouting"
+    
