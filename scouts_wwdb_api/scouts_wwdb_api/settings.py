fac16853ed07dab4e3c480b2cad251c33bba0615
Jeroen Wouters <jeroenwouters@inuits.eu>
2021-03-15 18:19:33 +0100

    Move static files and add STATICFILES_DIRS

--- scouts_wwdb_api/scouts_wwdb_api/settings.py
+++ scouts_wwdb_api/scouts_wwdb_api/settings.py
@@ -137,6 +137,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATICFILES_DIRS = [env.str("STATICFILES_EXPORT_WWDB"),]
+
 STATIC_URL = "static/"
 STATIC_ROOT = env.str("STATIC_ROOT")
 
