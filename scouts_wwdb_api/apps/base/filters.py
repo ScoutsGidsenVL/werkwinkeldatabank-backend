7ca17a2ec73302c0be861a1a6f9d94f96b63a70e
Wouter Vanden Hove <wouter@libranet.eu>
2024-03-10 03:51:52 +0100

    name is needed

--- scouts_wwdb_api/apps/base/filters.py
+++ scouts_wwdb_api/apps/base/filters.py
@@ -6,6 +6,13 @@ class ActiveFilterMixin(django_filters.FilterSet):
     active = django_filters.BooleanFilter(method="active_filter")
 
     def active_filter(self, queryset, name, value):
+        """Filter for active/inactive objects.
+        
+        The callable receives a QuerySet, 
+        the name of the model field to filter on, 
+        and the value to filter with. 
+        It should return a filtered Queryset.
+        """
         if value:
             return queryset.active()
         return queryset.inactive()
