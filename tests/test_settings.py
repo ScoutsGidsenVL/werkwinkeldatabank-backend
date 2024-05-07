4f144e199aafc497be2ce7cc3483e884a334eac0
Wouter Vanden Hove <wouter@libranet.eu>
2024-05-07 03:14:18 +0200

    fix test

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -3,13 +3,23 @@
 # pylint: disable=missing-function-docstring
 """Testing of module scouts_wwdb_api.settings."""
 
+import pytest
+
 
 def test_settings():
     # from scouts_wwdb_api import settings
-    import django
-    django.setup()
+    # see https://docs.djangoproject.com/en/4.2/topics/settings/#calling-django-setup-is-required-for-standalone-django-usage
 
+    import django
     from django.conf import settings
 
-    assert settings.DEBUG is True
+    django.setup()
 
+    assert settings.DEBUG is True  # see django_debug_mode-setting
+    assert settings.AUTH_USER_MODEL == "scouts_auth.User"
+
+
+@pytest.mark.urls("scouts_wwdb_api.urls")
+def test_swagger_url(client):
+    assert b"Scouts WWDB API" in client.get("/swagger/").content
+    assert b"Scouts WWDB API" in client.get("/swagger/").content
