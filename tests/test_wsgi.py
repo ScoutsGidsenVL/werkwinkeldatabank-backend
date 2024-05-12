6d3d1f88bd2314df20d56fb9ffa63885be2419f6
Wouter Vanden Hove <wouter@libranet.eu>
2024-05-12 22:50:43 +0200

    extra tests

---
+++ tests/test_wsgi.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=import-outside-toplevel
+# pylint: disable=missing-function-docstring
+"""Testing of module scouts_wwdb_api.wsgi."""
+
+
+def test_wsgi():
+    import django
+    from scouts_wwdb_api.wsgi import  application
+
+    assert application is not None
+    assert isinstance(application, django.core.handlers.wsgi.WSGIHandler)
+
+
+
+
