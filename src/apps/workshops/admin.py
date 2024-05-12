0d1a36fcab343dff70e457d98bf21513cf471306
Wouter Vanden Hove <wouter@libranet.eu>
2024-05-12 19:19:22 +0200

    enable models in django-admin

---
+++ src/apps/workshops/admin.py
@@ -0,0 +1,28 @@
+"""apps.admin.
+
+see https://codinggear.org/how-to-register-model-in-django-admin/
+
+"""
+import itertools as it 
+
+import django
+
+# import apps.workshops.models as workshops_models
+# django.contrib.admin.site.register(workshops_models.Workshop)
+# django.contrib.admin.site.register(workshops_models.Theme)
+
+models = it.chain(
+    # django.apps.apps.get_app_config("base").get_models(),
+    django.apps.apps.get_app_config("files").get_models(),
+    django.apps.apps.get_app_config("scouts_auth").get_models(),
+    django.apps.apps.get_app_config("workshops").get_models(),
+)
+
+
+# models = django.apps.apps.get_models()
+
+for model in models:
+    try:
+        django.contrib.admin.site.register(model)
+    except django.contrib.admin.sites.AlreadyRegistered:
+        pass
