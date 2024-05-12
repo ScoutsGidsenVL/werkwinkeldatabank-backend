c5e504b319daa1fce7184cbd8f1e9337c0b1300f
Wouter Vanden Hove <wouter@libranet.eu>
2024-05-12 19:16:38 +0200

    add login as super-user in ipython

--- etc/ipython/profile_default/startup/20-django.py
+++ etc/ipython/profile_default/startup/20-django.py
@@ -16,6 +16,7 @@ return-statements are not allowed.
 """
 print(f"Executing {__file__}")
 
+import os
 import django
 from django.conf import settings
 
@@ -29,10 +30,30 @@ import django.db as db
 import django.db.models as models
 import django.db.models as django_db_models
 import django.contrib.auth.models as django_contrib_auth_models
+import django.contrib.auth.models as djc_auth_models
 
-# import from django.contrib.auth.models import Group
 
 import apps.base.models as base_models
 import apps.files.models as files_models
 import apps.scouts_auth.models as scouts_auth_models
 import apps.workshops.models as workshops_models
+
+
+
+def login():
+    """Log in with superuser-credentials.
+
+    > bin/django-admin createsuperuser --username <username> --email <email>
+    > bin/django-admin createsuperuser
+
+    """
+    from django.contrib.auth import authenticate
+    superuser_username = os.getenv('DJANGO_SUPERUSER_USERNAME', "")
+    superuser_email = os.getenv('DJANGO_SUPERUSER_EMAIL', "")
+    superuser_password = os.getenv('DJANGO_SUPERUSER_PASSWORD', "")
+
+    su = authenticate(username=superuser_username, password=superuser_password)
+    assert su is not None
+    assert su.is_staff is True
+    assert su.is_superuser is True
+    assert su.is_active is True
